81c88729a84a30be132b9fde62c5e5525834fb89
Jonathan Brown <jbrown@acuity.social>
2022-08-30 15:59:30 +0700

    Build testnet with --release

--- scripts/testnet.sh
+++ scripts/testnet.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cargo build
-./target/debug/acuity --dev --port 30334 --ws-port 9946 -lruntime=debug
+cargo build --release
+./target/release/acuity --dev --port 30334 --ws-port 9946 -lruntime=debug
